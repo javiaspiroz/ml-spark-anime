1a688f69ab6276fffbb8327f82cd28663c7521dc
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-12-09 01:02:08 +0100

    creo dataframes
    e inicio de test y training

--- app.py
+++ app.py
@@ -7,6 +7,31 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
-file_obj = open("anime_spark_ml\dataset_valoraciones_anime\/anime.csv", encoding="utf-8")
-master = pd.read_csv(file_obj)
-print(master)
+animes = spark.read.csv("dataset_valoraciones_anime/anime.csv", header=True, inferSchema=True, sep=",", encoding="utf8",escape="\"")
+# animes.show()
+ratings = spark.read.csv("dataset_valoraciones_anime/rating_red.csv", header=True, inferSchema=True, sep=",")
+# ratings.show()
+
+# Unimos los dos CSVs para saber el tipo de anime
+ratings = ratings.join(animes,ratings.anime_id==animes.ID,"inner").select(ratings["*"],animes["Type"])
+ratings.show()
+# Separamos los ratings en pelis y series
+ratingsTV= ratings.filter(ratings['Type']=="TV")
+ratingsTV.show()
+ratingsMovies= ratings.filter(ratings['Type']=="Movie")
+ratingsMovies.show()
+ratings = [ratingsMovies,ratingsTV]
+
+for df in ratings:
+    # Dividimos los dataframes en test y training
+    (training,test) = df.randomSplit([0.8, 0.2])
+    # Entrenamos el modelo, se usa coldstart con drop para descartar Nan
+    als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="anime_id", ratingCol="rating", coldStartStrategy="drop")
+    model=als.fit(training)
+    
+    
+
+
+
+# Para terminar el proceso de Spark
+spark.stop()

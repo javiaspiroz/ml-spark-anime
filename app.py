8c324dc7d658b70d685987e76eb998458d97013a
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-12-11 14:54:40 +0100

    comento codigo

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import pandas as pd
-from pyspark.ml import recommendation
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
-from pyspark.sql import Row
-from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession
 
+# Función para crear los archivos .txt y HTML
 def guardar(df, name):
-    text_file = open('output/'+name+'.txt','w', encoding='utf8')
-    text_file.write(df.to_string())
-    text_file.close()
-    html = df.to_html()
-    html_file =open('output/'+name+".html", 'w',encoding='utf8')
-    html_file.write(html)
-    html_file.close()
+    txt = open('output/'+name+'.txt', 'w', encoding='utf8')
+    txt.write(df.to_string())
+    txt.close()
+    htmlDF = df.to_html()
+    html = open('output/'+name+".html", 'w', encoding='utf8')
+    html.write(htmlDF)
+    html.close()
 
-pd.set_option("display.max_columns", None)
+pd.set_option("display.max_columns", None) # Para imprimir completamente el datframe de pandas
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
-animes = spark.read.csv("dataset_valoraciones_anime/anime.csv", header=True, inferSchema=True, sep=",", encoding="utf8",escape="\"")
+# Leo los CSVs
+animes = spark.read.csv("dataset_valoraciones_anime/anime.csv", header=True, inferSchema=True, sep=",", encoding="utf8", escape="\"")
 # animes.show()
 ratings = spark.read.csv("dataset_valoraciones_anime/rating_red.csv", header=True, inferSchema=True, sep=",")
 # ratings.show()
@@ -27,26 +26,29 @@ ratings = spark.read.csv("dataset_valoraciones_anime/rating_red.csv", header=Tru
 (training,test) = ratings.randomSplit([0.8, 0.2])
 # Entrenamos el modelo, se usa coldstart con drop para descartar Nan
 als = ALS(maxIter=5, regParam=0.01, userCol="user_id", itemCol="anime_id", ratingCol="rating", coldStartStrategy="drop")
-model=als.fit(training)
+model = als.fit(training)
 # Evaluamos el modelo con RMSE
 predictions = model.transform(test)
 evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
-users = ratings.filter(ratings["user_id"]==666666)
-userSubsetRecs = model.recommendForUserSubset(users, 100)
-resultado = userSubsetRecs.first()['recommendations']
-recommendations=[]
-for i in resultado:
-    recommendations.append(i['anime_id'])
-dataframe=spark.createDataFrame(pd.DataFrame(recommendations, columns=["Anime_ID"]))
-result_ratings = dataframe.join(animes, [dataframe.Anime_ID==animes.ID], 'left').select('ID','Name','Japanese name','Type')
-results=result_ratings.drop('Anime_ID')
+users = ratings.filter(ratings["user_id"]==666666) # Es el ID del usuario EP (666666) para el que recomendamos
+userSubsetRecs = model.recommendForUserSubset(users, 100) #Tomamos 100 recomendaciones como nº suficiente par auqe haya 5 pelis y 5 series
+userRecs = userSubsetRecs.first()['recommendations']
+# Almacenamos las recomendaciones
+recs = []
+for i in userRecs:
+    recs.append(i['anime_id'])
 
-peliculas=results.filter(results['Type']=="Movie").toPandas()
-peliculas = peliculas[0:5]
+# Creamos un DF con el que unimos los datos de ambos CSVs
+df=spark.createDataFrame(pd.DataFrame(recs, columns=["Anime_ID"]))
+userRecsRats = df.join(animes, [df.Anime_ID==animes.ID], 'left').select('ID', 'Name', 'Japanese name', 'Type')
+recsFull = userRecsRats.drop('Anime_ID')
+# Separamos las películas
+peliculas=recsFull.filter(recsFull['Type']=="Movie").toPandas()
+peliculas = peliculas[0:5] # Cogemos solo las 1as
 print(peliculas)
 guardar(peliculas, "peliculas")
-
-series=results.filter(results['Type']=="TV").toPandas()
-series = series[0:5]
+# Separamos las series
+series = recsFull.filter(recsFull['Type']=="TV").toPandas()
+series = series[0:5] # Cogemos solo las 1as
 print(series)
 guardar(series, "series")
